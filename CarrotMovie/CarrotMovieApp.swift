b84dc352a5cf39c249486603a93525465fb2bce9
Karen Tran <karentran28@hotmail.com>
2025-11-20 12:06:27 -0800

    Initial Commit

---
+++ CarrotMovie/CarrotMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrotMovieApp.swift
+//  CarrotMovie
+//
+//  Created by Karen Tran on 2025-11-20.
+//
+
+import SwiftUI
+
+@main
+struct CarrotMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
